8368f34b8e2d0694aba617baab3ef71ecaedb9ee
Swad <SwadicalRag@users.noreply.github.com>
2016-01-05 16:19:53 +1100

    Framework and proof of concept

---
+++ lua/autorun/swadical.mui.lua
@@ -0,0 +1,65 @@
+require("bon")
+require("tfs")
+
+if SERVER then
+    AddCSLuaFile("includes/modules/bon.lua")
+    AddCSLuaFile("includes/modules/tfs.lua")
+    AddCSLuaFile("includes/modules/lustache.lua")
+    AddCSLuaFile("includes/modules/bxml.lua")
+    AddCSLuaFile("includes/modules/mui.lua")
+
+    local TemplateFS
+
+    local function recurseTemplateDir(path,loc)
+        local files,folders = file.Find(path.."*",loc)
+
+        for _,fileName in ipairs(files) do
+            TemplateFS:Write(fileName,file.Read(path..fileName,loc))
+        end
+
+        for _,folderName in ipairs(folders) do
+            TemplateFS:CreateDir(folderName)
+            TemplateFS:ChangeDir(folderName)
+            recurseTemplateDir(path..folderName.."/",loc)
+            TemplateFS:ChangeDir("..")
+        end
+    end
+
+    util.AddNetworkString("swadical.mUI.templateSender")
+
+    local function buildTemplateFS()
+        TemplateFS = TFS.New("Templates")
+        recurseTemplateDir("templates/","GAME")
+
+        local serialized = bON.serialize(TemplateFS:ToData())
+        local compressed = util.Compress(serialized)
+
+        net.Start("swadical.mUI.templateSender")
+            net.WriteData(compressed,#compressed)
+        net.Broadcast()
+    end
+
+    concommand.Add("mui_buildfs",buildTemplateFS)
+    buildTemplateFS()
+
+    timer.Create("swadical.mUI.rebuild",0.5,0,buildTemplateFS)
+
+    hook.Add("PlayerInitialSpawn","swadical.mUI.templateSender",function(ply)
+        local serialized = bON.serialize(TemplateFS:ToData())
+        local compressed = util.Compress(serialized)
+
+        net.Start("swadical.mUI.templateSender")
+            net.WriteData(compressed,#compressed)
+        net.Send(ply)
+    end)
+else
+    --include("includes/modules/mui.lua")
+
+    net.Receive("swadical.mUI.templateSender",function(len)
+        mUI.FS = TFS.FromData(bON.deserialize(util.Decompress(net.ReadData(len))))
+        hook.Run("mUI.Ready",mUI)
+
+        local template = mUI:FromTemplate("test.xml")
+        template:SetDraw(true)
+    end)
+end
--- lua/includes/modules/mui.lua
+++ lua/includes/modules/mui.lua
@@ -1,8 +1,100 @@
-include("bxml.lua")
-include("lustache.lua")
+require("bxml")
+require("lustache")
 
-mUI = {}
+mUI = mUI or {}
 
+mUI.templates = {}
+mUI.activeTemplates = {}
 
+function mUI:FromTemplate(name)
+    local results = self.FS:SearchFile(name,true,false)
+    if #results > 0 then
+        local template = {}
+        template.contents = self.FS:Read(results[1])
+
+        function template:buildTemplate()
+            self.template = lustache:render(self.contents,self.data)
+            self.XML = bXML:Parse(self.template)
+        end
+
+        template.lastDataTime = 0
+        template.templateUpdateInterval = 0.1
+        function template:getDataInternal()
+            if (SysTime() - self.lastDataTime) > self.templateUpdateInterval then
+                self.data = self:GetData()
+                self:buildTemplate()
+
+                self.lastDataTime = SysTime()
+            end
+
+            return self.data
+        end
+
+        function template:GetData()
+            return {}
+        end
+
+        function template:SetDraw(val)
+            self.draw = val
+        end
+
+        self.activeTemplates[#self.activeTemplates+1] = template
+
+        return template
+    else
+        error("Template "..path.." does not exist")
+    end
+end
+
+mUI.renderers = {}
+function mUI:renderNode(name,data)
+    if self.renderers[name] then
+        self.renderers[name](self.renderers,data)
+    else
+        error("Unknown render node "..data)
+    end
+end
+
+function mUI:parseRenderNode(name,tbl)
+    self:renderNode(name,tbl._data)
+
+    for name,data in pairs(tbl) do
+        if name == "_data" then continue end
+        self:parseRenderNode(name,data)
+    end
+end
+
+function mUI:Render(template)
+    template:getDataInternal()
+    for name,data in pairs(template.XML) do
+        if name == "_data" then continue end
+        self:parseRenderNode(name,data)
+    end
+end
+
+function mUI:RegisterRenderer(name,fn)
+    self.renderers[name] = fn
+end
+
+function mUI:parseColor(colorStr)
+    if colorStr:find("^rgba%(.-%)$") then
+        return Color(colorStr:match("^rgba%(([%d%.]-),([%d%.]-),([%d%.]-),([%d%.]-)%)$"))
+    elseif colorStr:find("^rgb(.-)$") then
+        return Color(colorStr:match("^rgb%(([%d%.]-),([%d%.]-),([%d%.]-)%)$"))
+    else
+        error("Bad color string")
+    end
+end
+
+mUI:RegisterRenderer("Box",function(renderers,data)
+    draw.RoundedBox(tonumber(data.cornerRadius or 0),tonumber(data.x),tonumber(data.y),tonumber(data.w),tonumber(data.h),mUI:parseColor(data.color))
+end)
+hook.Add("DrawOverlay","swadical.mUI.autoRender",function()
+    for _,template in ipairs(mUI.activeTemplates) do
+        if template.draw then
+            mUI:Render(template)
+        end
+    end
+end)
 
 return mUI
